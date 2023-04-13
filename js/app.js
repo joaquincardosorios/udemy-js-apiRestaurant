4616063c9a48efee8a36de4429fcad7cdda0840e
Joaquin Cardoso Rios <105686879+joaquincardosorios@users.noreply.github.com>
2023-04-13 12:20:30 -0400

    checkin-costumer

--- js/app.js
+++ js/app.js
@@ -3,3 +3,19 @@ let cliente = {
     hora:'',
     pedido: []
 }
+
+const btnGuardarCliente = document.querySelector('#guardar-cliente')
+btnGuardarCliente.addEventListener('click', guardarCliente)
+
+
+function guardarCliente() {
+    const mesa = document.querySelector('#mesa').value;
+    const hora = document.querySelector('#hora').value;
+
+    // Validar datos
+    if ([ mesa, hora ].some( campo => campo === '')){
+        console.log('alguno vacio')
+    } else{
+        console.log('todo bien')
+    }
+}
