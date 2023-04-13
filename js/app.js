dd69cb2d6a4e77c47ec695756cde011deb37ef33
Joaquin Cardoso Rios <105686879+joaquincardosorios@users.noreply.github.com>
2023-04-13 14:40:05 -0400

    show-API-info

--- js/app.js
+++ js/app.js
@@ -1,7 +1,20 @@
+/*
+Alternativamente:
+Se puede subir una base de datos local del archivo db.json, se debe tener instalado NodeJs y json-server
+Ejecutar: json-server --watch db.json --port 4000
+*/
+
+
 let cliente = {
     mesa: '',
     hora:'',
     pedido: []
+}
+
+const categorias = {
+    1: 'Comida',
+    2: 'Bebidas',
+    3: 'Postres'
 }
 
 const btnGuardarCliente = document.querySelector('#guardar-cliente')
@@ -36,8 +49,54 @@ function guardarCliente() {
     // Ocultar modal
     const modalFormulario = document.querySelector('#formulario');
     const modalBootstrap = bootstrap.Modal.getInstance(modalFormulario);
-    modalBootstrap.hide()
+    modalBootstrap.hide();
+
+    // Mostrar secciones
+    mostrarSecciones();
+
+    // Obtener platillos de la API
+    obtenerPlatillos();
 
 
-    console.log(cliente);
+}
+
+function mostrarSecciones(){
+    const seccionesOcultas = document.querySelectorAll('.d-none');
+    seccionesOcultas.forEach( seccion => seccion.classList.remove('d-none'));
+}
+
+function obtenerPlatillos(){
+    const url = 'https://64382121c1565cdd4d66ebc4.mockapi.io/api/v1/platillos';
+    fetch(url)
+        .then(respuesta => respuesta.json())
+        .then(platos => mostrarPlatillos(platos))
+        .catch( error => console.log(error))
+}
+
+function mostrarPlatillos(platos){
+    const contenido = document.querySelector('#platillos .contenido');
+
+    platos.forEach(plato => {
+        const row = document.createElement('DIV');
+        row.classList.add('row', 'py-3', 'border-top');
+
+        const nombre = document.createElement('DIV');
+        nombre.classList.add('col-md-4');
+        nombre.textContent = plato.nombre;
+
+        const precio = document.createElement('DIV');
+        precio.classList.add('col-md-3', 'fw-bold');
+        precio.textContent = `$ ${plato.precio}` ;
+
+        const categoria = document.createElement('DIV');
+        categoria.classList.add('col-md-3');
+        categoria.textContent = categorias[ plato.categoria ];
+
+
+        row.appendChild(nombre);
+        row.appendChild(precio);
+        row.appendChild(categoria);
+
+        contenido.appendChild(row);
+    });
 }
